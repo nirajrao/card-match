c172789a8093248e6863fcd69ce3153e6e46aba6
Niraj Rao <niraj@Nirajs-MacBook-Pro.local>
2022-04-23 11:40:11 -0700

    Restructure test, add Github Action

--- internal/cmd/test/root.go
+++ internal/cmd/test/root.go
@@ -16,18 +16,22 @@ func Cmd() *cobra.Command {
 			server := api.NewDefaultServer()
 			go server.StartApplicationServer()
 
-			resp, err := http.Get("http://localhost:8080/ping")
+			// Add retry policy in case the server is slow to start up.
+			for i := 0; i <= 50; i++ {
+				resp, err := http.Get("http://localhost:8080/ping")
 
-			if err != nil {
-				panic(err)
+				if err != nil || resp.StatusCode != 200 {
+					continue
+				}
+
+				if resp.StatusCode == 200 {
+					fmt.Print("Received a succesful response")
+					return
+				}
+
 			}
 
-			if resp.StatusCode != 200 {
-				panic("Did not receive a successful response")
-			}
-
-			fmt.Print("Received a successful response")
-
+			panic("Did not receive a successful response")
 		}}
 
 	return cmd
